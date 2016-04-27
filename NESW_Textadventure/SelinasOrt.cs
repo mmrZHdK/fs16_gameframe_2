1ac5daf2675953907da2c729bdc0de1ad2ca2273
selinaZHdK <selina.capol@zhdk.ch>
2016-04-27 16:47:45 +0200

    CustomCommand
    Test

--- NESW_Textadventure/SelinasOrt.cs
+++ NESW_Textadventure/SelinasOrt.cs
@@ -17,12 +17,32 @@ namespace TextAdventure
 				
 		}
 
-		public override bool IstCustomCommand( string in_kommando )
-		{
-			return false;
+
+		//Befehle, die nur in meinem Raum möglich sind
+		public override bool IstCustomCommand(string in_kommando){
+
+			return ((in_kommando == "look") || (in_kommando == "take food") || (in_kommando == "squeeze")) ;
+
+
+
+
 		}
-		public override void BehandleCustomCommand( string in_kommando )
-		{
+
+		public override void BehandleCustomCommand(string in_kommando){
+			if (in_kommando == "look") {
+				string antwort = "You take a closer look at the walls and find a crack in the east wall! It's big enough for a human to squeeze through. ";
+				Console.WriteLine (antwort);
+			} else if (in_kommando == "take food") {
+				string antwort = "You take a can with you. It's tagged 'ravioli'. ";
+				Console.WriteLine (antwort);
+			} else if (in_kommando == "squeeze") {
+				string antwort = "You managed to squeeze through the crack. ";
+				Console.WriteLine (antwort);
+				// Marcels Raum aufrufen
+				//				MarcellosOrt.LosGehts ();
+
+
+			}
 		}
 	}
 }
