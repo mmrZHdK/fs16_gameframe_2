255c7561698279b7fada145ccf3f27fbf2e58687
Magnus Rembold <magnus.rembold@zhdk.ch>
2016-04-27 14:58:11 +0200

    Neue Klasse Ding dazu

---
+++ NESW_Textadventure/Ding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TextAdventure;
+
+namespace TextAdventure
+{
+	public class Ding
+	{
+		// -- Properties, die alle Dinge haben sollen
+		protected string name = "untitled item";
+
+		public Ding ( string in_name )
+		{
+			name = in_name;
+		}
+	}
+}
+
