30efa6b15f564865a06113571c6be87fa2a4dea3
tobanzhdk <tobiasdupuch@Tobiass-MacBook-Pro.local>
2016-05-04 16:12:10 +0200

    ding gemacht

---
+++ NESW_Textadventure/TobiasDing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TextAdventure;
+
+namespace DasTextAdventure
+{
+	public class TobiasDing : Ding
+	{
+		name = "plummer";
+
+		//Konstruktor:
+		//public TobiasDing ()
+		//{
+		//}
+	}
+}
+
