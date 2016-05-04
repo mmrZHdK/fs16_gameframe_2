58609112f3111bd45bd49932a71c0f7efa98a300
MyriamAssuncao <itz@MacBook-Pro.local>
2016-05-04 14:12:09 +0200

    Zusatzinfos

--- NESW_Textadventure/MyriamsOrt.cs
+++ NESW_Textadventure/MyriamsOrt.cs
@@ -7,14 +7,19 @@ namespace TextAdventure
 	{
 		public MyriamsOrt ()
 		{
+			// Beschreibung was der Spieler in meinem Ort bekommt
 			name = "kitchen";
 			bezug = "in";
 			beschrieb = "It seems that this is the kitchen of the house.\n";
 			beschrieb += "It still smells of the last dinner but there is no food left." ;
+			beschrieb += "Only in the middle of the table stands a bowl with 4 red apples in it.";
 			beschrieb += "Everything glares and seems clean.";
 			beschrieb += "You see a dirty knife that is dangerously balancing over the kitchen table. ";
 			beschrieb += "Ping!";
 			beschrieb += "It fell on the floor and points to a woody old door.";
+
+			beschrieb += "The woody old door leads you to the pantry.";//South. Speisekammer von Selina
+			beschrieb += "The old door leads you to the dining room ";//West. Speisesaal von Nicole
 
 		}
 		public override bool IstCustomCommand( string in_kommando )
