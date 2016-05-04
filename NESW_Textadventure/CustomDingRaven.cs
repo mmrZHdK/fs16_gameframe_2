c96145c1d2530f6642c7083d2d1d81f52dc3c5eb
r00f <d.wuersch@gmail.com>
2016-05-04 16:04:43 +0200

    Ding subklasse Raven
    Ein spezielles Ding

---
+++ NESW_Textadventure/CustomDingRaven.cs
@@ -0,0 +1,16 @@
+using System;
+using TextAdventure;
+
+namespace TextAdventure
+{
+	public class Raven : Ding
+	{
+		public Raven ()
+		{
+			name = "Raven";
+			beschrieb = "It flaps its wings and seems unhappy.";
+
+		}
+	}
+
+}
