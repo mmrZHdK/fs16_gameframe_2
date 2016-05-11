2c86032bf73d51c1d5df5fc189a58376fc7401cd
selinaZHdK <selina.capol@zhdk.ch>
2016-05-11 13:43:42 +0200

    SelinaDing
    Ding (rat)

--- NESW_Textadventure/SelinasOrt.cs
+++ NESW_Textadventure/SelinasOrt.cs
@@ -14,13 +14,18 @@ namespace TextAdventure
 			beschrieb += "In the south is the door to the kitchen. ";
 			beschrieb += "All other walls are covered with shelves which are packed with canned food. ";
 			beschrieb += "You see a rat sneaking around the shelves. ";
+
+
+			//Dinge in meinem Raum
+			Ding ravioli = new Ding ("canned ravioli");
+			this.VerknuepfeDing(ravioli);
+			Ding gab = new Ding ("crack in the wall");
+			this.VerknuepfeDing(gab);
+
+			//Spezial Dinge
+			SelinaDing ratte = new SelinaDing(name);
+			this.VerknuepfeDing (ratte);
 		}
-
-		//Dinge in meinem Raum
-		Ding rat = new Ding ("rat");
-		Ding ravioli = new Ding ("Canned Ravioli");
-		Ding gab = new Ding ("crack in the wall");
-
 
 		//Befehle, die nur in meinem Raum möglich sind
 		public override bool IstCustomCommand(string in_kommando){
