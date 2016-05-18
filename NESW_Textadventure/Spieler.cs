009c576037c9444f5d654ae1082d29c94b809725
Julian Schönbächler <info@creative-forge.ch>
2016-05-18 16:21:09 +0200

    Inventar-Liste und basic Inventar Methoden.

--- NESW_Textadventure/Spieler.cs
+++ NESW_Textadventure/Spieler.cs
@@ -1,17 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 using TextAdventure;
 
 namespace TextAdventure
 {
 	public class Spieler
 	{
+		// Inventar erstellen, vom abstrakten Datentyp 'List'
+		List<Ding> inventar = new List<Ding>();
+
 		public Spieler ()
 		{
 			// was hier passieren soll, sehen wir noch
 		}
 
 		public void InventarAusgeben() {
-			Console.WriteLine ("You've got nothing in your pocket, sorry.");
+
+			// Wenn nichts im Inventar
+			if(inventar.Count == 0)
+			{
+				// Ausgabe
+				Console.WriteLine ("You've got nothing in your pocket, sorry.");
+			}
+			else
+			{
+				// Ansonsten Inventar auflisten
+				Console.WriteLine("Inventory content:");
+
+				// Schleife durchs Inventar
+				foreach(Ding ding in inventar)
+				{
+					// Der Name jedes Dings ausgeben
+					Console.WriteLine("-- " + ding.name);
+				}
+			}
+		}
+
+		// Schmeiss Dinge in Inventar
+		public void ZuInventar(Ding ding) {
+			inventar.Add(ding);
+		}
+
+		// Ist im Inventar vorhanden?
+		public bool IstImInventar(Ding ding) {
+			return inventar.Contains(ding);
+		}
+
+		// Aus Inventar entfernen
+		public bool AusInventar(Ding ding) {
+			return inventar.Remove(ding);
 		}
 	}
 }
