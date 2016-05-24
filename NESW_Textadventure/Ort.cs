62996d1af8d04f1eb501f396bf16abaffe7ddb89
Aleksandra Iakusheva <sashok.net@mail.ru>
2016-05-24 21:04:40 +0200

    Inventar
    Ich habe ein paar funktionen in Ding Ort geschrieben um die Dinge aus
    Raumen zu loeschen. Und in Spieler Klasse einen Dictionary fuer unsere
    Sache gemacht und auch die Funktion um diese Sache in Inventar zu
    platzieren.

--- NESW_Textadventure/Ort.cs
+++ NESW_Textadventure/Ort.cs
@@ -174,6 +174,9 @@ namespace TextAdventure
 			// Dictionary für dinge
 			dinge[ in_ding.name ] = in_ding;
 		}
+		public void RemoveFromRoom(Ding in_ding){    //function for removing things from rooms
+			dinge.Remove (in_ding.name);
+		}
 
 		public bool IstDingKommando( string in_kommando ) {
 
@@ -254,7 +257,17 @@ namespace TextAdventure
 						Console.WriteLine ("-- Juchu, " + name + " ist identifiziert, Kommando weitergeben");
 						Ding ding = dingEintrag.Value;
 						ding.BehandleKommando (kommandoTeil);
+						if (kommandoTeil == "take") {
+							RemoveFromRoom (ding);         //remove item from room
+							spieler.PlaceInInventar(ding);   //add item in inventory
+							Console.WriteLine (ding.name + " is placed in inventory.");
+						
+						} else {
+							ding.BehandleKommando (kommandoTeil);
+						}
+
 						identifiziert = true;
+						break; 						//go out from the loop
 					}
 				}
 
