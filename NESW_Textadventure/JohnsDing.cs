1314484a31ba546f1b8c66c88649bbe9c703da21
JohannesKoeberle <itz@users-MacBook-Pro.local>
2016-05-25 01:40:31 +0200

    Ding von John

---
+++ NESW_Textadventure/JohnsDing.cs
@@ -0,0 +1,16 @@
+using System;
+using TextAdventure;
+
+namespace TextAdventure
+{
+	public class JohnsDing: Ding
+	{
+		
+		public JohnsDing () 
+		{
+			name = "wooden chest";
+			beschrieb = "It's wooden and old.";
+		}
+	}
+}
+
